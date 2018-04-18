4ea557dd97591cd824deb7d78fb4aeed790c58ff
itggot-lukas-persson <lukas.persson@itggot.se>
2018-04-18 13:57:35 +0200

    min_of_three

---
+++ lib/min_of_three.rb
@@ -0,0 +1,15 @@
+def min_of_three(num1, num2, num3)
+    smallest = 0
+    
+    if num1 > num2
+        smallest = num2
+    else 
+        smallest = num1
+    end
+
+    if smallest > num3
+        return num3
+    else
+        return smallest
+    end
+end
