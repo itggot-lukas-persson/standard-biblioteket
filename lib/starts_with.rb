27b70fdc7f83ad952be33dd20e050393f97d9114
itggot-lukas-persson <lukas.persson@itggot.se>
2018-05-02 13:47:09 +0200

    starts_with

---
+++ lib/starts_with.rb
@@ -0,0 +1,11 @@
+# Tar en sträng och ett tecken som input och avgör om strängen börjar på det tecknet
+
+
+
+def starts_with(string, character)
+    if string[0] == character
+        return true
+    else 
+        return false
+    end
+end
