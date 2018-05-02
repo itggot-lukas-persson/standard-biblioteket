32e028c53d2a9b6ac4b9f83fbc635d9eaa8aa03c
itggot-lukas-persson <lukas.persson@itggot.se>
2018-05-02 13:51:21 +0200

    ends_with

---
+++ lib/ends_with.rb
@@ -0,0 +1,11 @@
+# Tar en sträng och ett tecken som input och avgör om strängen slutar på det tecknet
+
+
+
+def ends_with(string, character)
+    if string[-1] == character
+        return true
+    else 
+        return false
+    end
+end
